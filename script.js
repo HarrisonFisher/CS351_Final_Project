ec719407e344ae6104d61994798c600446a60d6d
Harrison Fisher <HarrisonFisher2018@gmail.com>
2023-05-05 21:59:11 -0700

    Fixed Serving Size

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const btn = document.getElementById("btn");
 btn.addEventListener("click", function() {
     const servings = document.getElementById("servings").value;
-    document.getElementById("spaghetti").textContent = 1 * servings;
-    document.getElementById("tomatoes").textContent = 1 * servings;
-    document.getElementById("garlic").textContent = 3 * servings;
-    document.getElementById("oil").textContent = 2 * servings;
+    document.getElementById("spaghetti").textContent = (1/4) * servings;
+    document.getElementById("tomatoes").textContent = (1/4) * servings;
+    document.getElementById("garlic").textContent = (3/4) * servings;
+    document.getElementById("oil").textContent = (2/4) * servings;
 });
